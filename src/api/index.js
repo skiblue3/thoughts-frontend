f2d3832d90b2d15b4316f983085bcd2f032d76a4
skiblue3 <myzticblue126@gmail.com>
2022-05-11 21:50:51 +0530

    added layout for auth

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'https://thoughts-project-03.herokuapp.com/api/posts';
+// const url = 'https://thoughts-project-03.herokuapp.com/api/posts';
+const url = 'http://localhost:5000/api/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
